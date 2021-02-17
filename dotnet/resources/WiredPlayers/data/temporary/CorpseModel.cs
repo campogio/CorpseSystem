d25c1f20eb91c6f67d0e523757bde03d55f41506
Giordano Campogiani <campogio@hotmail.it>
2021-02-17 16:10:51 +0100

    Initial Commit
    Basic version, add corpse and state time of death in user chat(To be removed)

---
+++ dotnet/resources/WiredPlayers/data/temporary/CorpseModel.cs
@@ -0,0 +1,29 @@
+﻿using GTANetworkAPI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredPlayers.data.temporary
+{
+    class CorpseModel
+    {
+        public List<HitModel> HitList { get; set; }
+        public Vector3 Location { get; set; }
+        public Vector3 Rotation { get; set; }
+        public String Name { get; set; }
+        public GTANetworkAPI.Object Model { get; set; }
+        public DateTime deathTime { get; set; }
+
+
+        public CorpseModel(List<HitModel> hitList,Vector3 location,Vector3 rotation,String name) 
+        {
+            HitList = hitList;
+            Location = location;
+            Rotation = rotation;
+            Name = name;
+            Model = NAPI.Object.CreateObject(1165866977, location, rotation);
+            deathTime = DateTime.Now;
+
+        }
+    }
+}
---
+++ dotnet/resources/WiredPlayers/data/temporary/HitModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WiredPlayers.data.temporary
+{
+    class HitModel
+    {
+        public ulong Weaponhash;
+        public ulong Boneidx;
+        public int Damage;
+        public string BoneString;
+        public HitModel(ulong weaponhash, ulong boneidx, int damage, string bonestring)
+        {
+            // Initialize the required values
+            Weaponhash = weaponhash;
+            Boneidx = boneidx;
+            Damage = damage;
+            BoneString = bonestring;
+        }
+    }
+}
